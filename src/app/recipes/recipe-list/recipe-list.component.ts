e4b05abae18342d6891753370c30f8ade87d9658
Daniel Stoehr <daniel.stoehr@boschrexroth.de>
2023-04-17 13:19:31 +0200

    implement data binding and format with prettier

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.scss']
+  styleUrls: ['./recipe-list.component.scss'],
 })
 export class RecipeListComponent {
-  recipes: Recipe[] = [new Recipe('A Test Recipe', 'this is simply a test', 'https://cdn.pixabay.com/photo/2014/12/21/23/28/recipe-575434_960_720.png')]
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+  recipes: Recipe[] = [
+    new Recipe(
+      'A Test Recipe',
+      'this is simply a test',
+      'https://cdn.pixabay.com/photo/2014/12/21/23/28/recipe-575434_960_720.png'
+    ),
+    new Recipe(
+      'Another Test Recipe',
+      'this is simply a test',
+      'https://cdn.pixabay.com/photo/2014/12/21/23/28/recipe-575434_960_720.png'
+    ),
+  ];
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
 }
--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  Output,
+  ViewChild,
+} from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
-  styleUrls: ['./shopping-edit.component.scss']
+  styleUrls: ['./shopping-edit.component.scss'],
 })
 export class ShoppingEditComponent {
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
+  @ViewChild('nameInput') nameInput: ElementRef;
+  @ViewChild('amountInput') amountInput: ElementRef;
 
+  onAddItem() {
+    this.ingredientAdded.emit({
+      name: this.nameInput.nativeElement.value,
+      amount: this.amountInput.nativeElement.value,
+    });
+  }
 }
